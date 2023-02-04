b7046e1446b20cbf3bd42576b22065e9b38fce46
Josua <josuasitanggang51@gmail.com>
2023-02-04 10:10:10 +0700

    fix gap

--- src/components/TriedParticipants.tsx
+++ src/components/TriedParticipants.tsx
@@ -7,7 +7,7 @@ const TriedParticipants= () =>{
                 <p className="mb-4">Lots of comments about us, here's what they said</p>
                 </div>
                 
-                <div className="flex flex-col justify-center items-center mx-auto md:flex-row md:p-[15px] md:flex md:items-center md:mx-auto  ">
+                <div className="flex flex-col justify-center items-center mx-auto md:flex-row md:p-[15px] md:flex md:items-center md:mx-auto md:gap-14 ">
                     <div className="w-[70%] border-2 border-inherit p-5 md:w-[400px] mb-11">
                         <img src="https://media.licdn.com/dms/image/D5603AQEn7xehQFYdEw/profile-displayphoto-shrink_800_800/0/1671607292631?e=1680739200&v=beta&t=H9002quy8PIpjeF_y65nOMkzaIxrkwB5TcIPhAj1dWE" alt=""  className="w-[100px] h-[100px] overflow-hidden rounded-full mb-2"/>    
                         <p className="mb-3 font-bold">Edwin Tantawi</p>
@@ -20,7 +20,7 @@ const TriedParticipants= () =>{
                         <p>it's very easy for me who work like us,in between work breaks can look for recipes here and make     them at home. So that we can provide the best food for our family</p>
                     </div>
 
-                    <div className="w-[70%] border-2 border-inherit p-5 md:w-[400px]">
+                    <div className="w-[70%] border-2 border-inherit p-5 md:w-[400px] mb-11">
                         <img src="https://media.licdn.com/dms/image/D5635AQGptaG36URXdA/profile-framedphoto-shrink_800_800/0/1662459098792?e=1676080800&v=beta&t=IluC01N1CU1Yf4gKOGKmScGs2BiPgbf5Hq00tYJ2VFs" alt="" className="w-[100px] h-[100px] overflow-hidden rounded-full mb-2"/>
                         <p className="mb-3 font-bold">Diki Aditama</p>
                         <p>it's very easy for me who work like us,in between work breaks can look for recipes here and make     them at home. So that we can provide the best food for our family</p>
