eede9d4cc59395eaae2bda0182f02140fbefb85c
Josua <josuasitanggang51@gmail.com>
2023-02-04 10:06:01 +0700

    fix responsive desain

--- src/components/TriedParticipants.tsx
+++ src/components/TriedParticipants.tsx
@@ -7,14 +7,14 @@ const TriedParticipants= () =>{
                 <p className="mb-4">Lots of comments about us, here's what they said</p>
                 </div>
                 
-                <div className="flex flex-col justify-center items-center gap-12 mx-auto md:flex-row md:p-[15px] md:flex md:items-center md:mx-auto">
-                    <div className="w-[70%] border-2 border-inherit p-5 md:w-[400px]">
+                <div className="flex flex-col justify-center items-center mx-auto md:flex-row md:p-[15px] md:flex md:items-center md:mx-auto  ">
+                    <div className="w-[70%] border-2 border-inherit p-5 md:w-[400px] mb-11">
                         <img src="https://media.licdn.com/dms/image/D5603AQEn7xehQFYdEw/profile-displayphoto-shrink_800_800/0/1671607292631?e=1680739200&v=beta&t=H9002quy8PIpjeF_y65nOMkzaIxrkwB5TcIPhAj1dWE" alt=""  className="w-[100px] h-[100px] overflow-hidden rounded-full mb-2"/>    
                         <p className="mb-3 font-bold">Edwin Tantawi</p>
                         <p>it's very easy for me who work like us,in between work breaks can look for recipes here and make     them at home. So that we can provide the best food for our family</p>
                     </div>
 
-                    <div className="w-[70%] border-2 border-inherit p-5 md:w-[400px]">
+                    <div className="w-[70%] border-2 border-inherit p-5 md:w-[400px] mb-11">
                         <img src="https://media.licdn.com/dms/image/C5103AQGMuJKVUqh2jw/profile-displayphoto-shrink_800_800/0/1527101226911?e=1680739200&v=beta&t=W7pju7TQ8Q_ubN51Fbk-u7Sy7mlr27S9R3IyZ4lDLnI"  alt="" className="w-[100px] h-[100px] overflow-hidden rounded-full mb-2"/>
                         <p className="mb-3 font-bold">Andrew Tarigan</p>
                         <p>it's very easy for me who work like us,in between work breaks can look for recipes here and make     them at home. So that we can provide the best food for our family</p>
