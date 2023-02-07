cce3ce3944dc5fea07341ae2bdff2af5a39a9543
Josua <josuasitanggang51@gmail.com>
2023-02-07 19:15:59 +0700

    add routing recipes

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import ContributorParticipant from './components/ContributorParticipant';
-import Footer from './components/Footer';
-import ImageBanner from './components/ImageBanner';
-import Navbar from './components/Navbar';
-import Participant from './components/Participant';
-import PopularRecipe from './components/PopularRecipe';
-import TriedParticipants from './components/TriedParticipants';
-
-interface Data {
-    id:number,
-    name: string,
-    image:string,
-
-}
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import PopularRecipes from "./routes/PopularRecipes";
 
 function App() {
-    const [api, setApi] = useState<Data[]>([]);
 
-    const handleAPI = async () => {
-        const data = await fetch('https://api.spoonacular.com/food/search?apiKey=013ce5402fe84f44a62e3d3bbee25cbe&number=100');
-        const response = await data.json();
-        setApi(response.searchResults[0].results);
-
-    }
-
-    useEffect(() => {
-        handleAPI();
-    }, [])
-
-    return (
-        <div>
-            <Navbar/>
-            <Participant/>
-            <ContributorParticipant/>
-            <PopularRecipe/>
-            <TriedParticipants/>
-            <ImageBanner/>
-            <Footer/>
-            {/* {api.length > 0 && api.map((data) => {
-                return (
-                    <div key={data.id}>
-                        {data.name}
-                       <img src={`${data.image}`} alt="" />
-                        </div>
-                )
-            })} */}
-        </div>
+    return(
+        <>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/recipes" element={<PopularRecipes/>}/>
+        </Routes>
+        </>
     )
+
 }
 
 export default App;
