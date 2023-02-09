aae0f6ddf13f1390c416dca47d66bf9b7f355b7a
Josua <josuasitanggang51@gmail.com>
2023-02-09 09:16:41 +0700

    fix design breakpoints

--- src/components/NavbarContent.tsx
+++ src/components/NavbarContent.tsx
@@ -14,14 +14,14 @@ const NavbarContent = ({
   return (
     <div className="flex flex-col mt-[78px] md:mt-[84px] pb-10 mx-auto pl-5 items-center justify-center md:flex md:flex-row">
       <div className="mr-10">
-        <h1 className="text-5xl font-mono">{title}</h1>
-        <p className="text-lg mt-2 font-serif">{descriptionProduct}</p>
-        <p className="text-lg  font-serif">{descriptionName}</p>
+        <h1 className="text-4xl sm:text-5xl font-mono">{title}</h1>
+        <p className="text-base sm:text-lg mt-2 font-serif">{descriptionProduct}</p>
+        <p className="text-base sm:text-lg font-serif">{descriptionName}</p>
       </div>
 
       <div>
         <img
-          className="w-[400px] h-[400px] overflow-hidden rounded-full bg-red "
+          className="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] overflow-hidden rounded-full bg-red "
           src={`${image}`}
           alt="image_photos"
         />
